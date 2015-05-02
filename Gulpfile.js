7cedce038aae98626d5d3b9fa275f263853486ee
soulteary <soulteary@gmail.com>
2015-05-02 23:05:10 +0800

    init

---
+++ Gulpfile.js
@@ -0,0 +1,61 @@
+var gulp = require('gulp');
+
+var amdOptimize = require("amd-optimize");
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var path = require('path');
+var rename = require("gulp-rename");
+var dirSync = require('gulp-directory-sync');
+var minifyCSS = require('gulp-minify-css');
+
+
+gulp.task("build", ["style:build", "script:build"], function () {});
+gulp.task("style:build", ["style:less", "style:copy-lib", "style:copy-font"], function () {});
+gulp.task("script:build", ["script:copy-lib"], function () {});
+
+gulp.task("scripts:index", function () {
+
+    return gulp.src("./src/**/*.js")
+        .pipe(amdOptimize("index", {
+            exclude: ['jquery']
+        }))
+        .pipe(concat("index.js"))
+        .pipe(gulp.dest("dist/js"));
+
+});
+
+
+// 处理less文件并压缩输出
+gulp.task("style:less", function () {
+    return gulp.src("./src/less/moe.less")
+        .pipe(less({paths: [path.join(__dirname, 'less', 'includes')]}))
+        .pipe(minifyCSS())
+        .pipe(rename("css/style.min.css"))
+        .pipe(gulp.dest("./dist"));
+});
+// 将依赖的base样式文件复制到输出目录
+gulp.task("style:copy-lib", function () {
+    return gulp.src("./src/css/lib/*.css")
+        .pipe(gulp.dest("dist/css/lib"));
+});
+// 复制依赖的字体文件
+gulp.task("style:copy-font", function () {
+    return gulp.src("./src/fonts/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+// 复制依赖的脚本文件
+gulp.task("script:copy-lib", function () {
+    return gulp.src("./src/js/lib/*.js")
+        .pipe(gulp.dest("dist/js/lib"));
+});
+
+
+// 同步输出文件到Demo目录
+gulp.task("demo:sync", function () {
+    gulp.src('')
+        .pipe(dirSync("src/html", 'demo', {printSummary: true}))
+        .pipe(dirSync("dist", 'demo/assets', {printSummary: true}))
+        .on('error', function (e) {
+            console.log(e);
+        });
+});
---
+++ README.md
@@ -0,0 +1,12 @@
+## 项目说明
+
+
+
+## Includes:
+
+- [Requirejs v2.1.17](https://github.com/jrburke/requirejs/tree/2.1.17)
+- [jQuery v2.1.4](https://github.com/jquery/jquery/tree/2.1.4)
+- [jQuery v1.11.3](https://github.com/jquery/jquery/tree/1.11.3)
+- [Bootstrap v3.3.4](https://github.com/twbs/bootstrap/tree/v3.3.4)
+
+
